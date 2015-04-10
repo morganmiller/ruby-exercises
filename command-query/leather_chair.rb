eaf291da3f53d204f43e6410bdf1958d8ec6efa0
Morgan Miller <m.miller722@gmail.com>
2015-04-09 18:22:10 -0600

    tires and chairs

---
+++ command-query/leather_chair.rb
@@ -0,0 +1,17 @@
+class LeatherChair
+
+  attr_accessor :faded
+
+  def initialize
+    @faded = false
+  end
+
+  def faded?
+    @faded
+  end
+
+  def expose_to_sunlight
+    @faded = true
+  end
+
+end
---
+++ command-query/tire.rb
@@ -0,0 +1,17 @@
+class Tire
+
+  attr_accessor :flat
+
+  def initialize
+    @flat = false
+  end
+
+  def flat?
+    @flat
+  end
+
+  def blow_out
+    @flat = true
+  end
+
+end
